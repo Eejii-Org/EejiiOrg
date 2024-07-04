0c064710e901dd3f8bc2d17541eff799e7eaeb0b
Enhmunh-E <enhuush0704@gmail.com>
2024-07-04 17:25:59 +0800

    donation modal front

--- src/components/home/donate-modal.tsx
+++ src/components/home/donate-modal.tsx
@@ -1,13 +1,162 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { Button } from "../button";
+import { Input } from "../input";
+
 export const DonateModal = () => {
+  const [donateOpen, setDonateOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [value, setValue] = useState<number | string>(10000);
+  const [selectedPyament, setSelectedPayment] = useState<"monthly" | "onetime">(
+    "onetime"
+  );
   return (
     <>
-      <button className="px-[24px] py-[12px] text-base text-white font-semibold md:hover:bg-[#87B7BF] rounded-xl transition-all bg-primary">
+      <button
+        onClick={() => setDonateOpen(true)}
+        className="px-[24px] py-[12px] text-base text-white font-semibold md:hover:bg-[#87B7BF] rounded-xl transition-all bg-primary"
+      >
         Donate
       </button>
-      <div></div>
+      <div
+        className={`fixed top-1/2 left-1/2 ${
+          donateOpen ? "fixed flex" : "hidden"
+        } z-10 gap-6 w-full md:w-[1080px] -translate-x-1/2 -translate-y-1/2`}
+      >
+        <div className="rounded-2xl bg-white overflow-hidden flex flex-col gap-8">
+          <div className="relative h-80">
+            <Image
+              src="/assets/home/donatebanner.webp"
+              alt="Donatebanner"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <div className="p-4 flex flex-col gap-6">
+            <Image
+              src="/assets/logo.png"
+              alt="EejiiIcon"
+              className="object-contain"
+              height={42}
+              width={168}
+            />
+            <div className="flex flex-col gap-4">
+              <h1 className="text-xl font-semibold">
+                Бидний үйл ажиллагаанд хандив өргөснөөр
+              </h1>
+              <p>
+                Cүүлийн жилүүдэд сайн дурын ажил хийх хүсэл сонирхолтой
+                хүмүүсийн тоо эрчимтэй өсөж байгаа нь гайхалтай билээ. Энэ нь
+                бид бусдын төлөө сэтгэлтэй болж өсөж, хүмүүжиж байгаагийн
+                илэрхийлэл юм.{" "}
+              </p>
+            </div>
+            <h2 className="font-semibold">Бидэнд хандив өргөсөнд баярлалаа!</h2>
+          </div>
+        </div>
+        <div className="p-8 rounded-2xl min-w-[400px] flex-1 flex flex-col justify-between gap-8 bg-white">
+          <div className="flex items-center justify-center">
+            <Image
+              src="/assets/logo.png"
+              alt="EejiiIcon"
+              className="object-contain"
+              height={42}
+              width={168}
+            />
+          </div>
+          <div className="flex flex-col flex-1 gap-4">
+            <div className="flex flex-row border rounded-xl box-border mb-8">
+              <button
+                onClick={() => setSelectedPayment("onetime")}
+                className={`flex-1 py-3 box-border ${
+                  selectedPyament == "onetime"
+                    ? "font-semibold text-black  border border-primary bg-[#EBF3F4] rounded-xl"
+                    : "font-medium text-black/70"
+                }`}
+              >
+                Нэг удаа
+              </button>
+              <button
+                onClick={() => setSelectedPayment("monthly")}
+                className={`flex-1 py-3 pr-2 box-border flex items-center justify-center gap-1 ${
+                  selectedPyament == "monthly"
+                    ? "font-semibold text-black border border-primary bg-[#EBF3F4] rounded-xl"
+                    : "font-medium text-black/70"
+                }`}
+              >
+                <HeartIcon />
+                Сар бүр
+              </button>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+              {[5000, 10000, 20000, 50000, 100000, 500000].map(
+                (tugrug, index) => (
+                  <button
+                    onClick={() => setValue(tugrug)}
+                    className="py-[10px] rounded-lg border border-[#CCCCCC] flex items-center justify-center hover:bg-[#CCCCCC] transition-all"
+                    key={index}
+                  >
+                    ₮{tugrug}
+                  </button>
+                )
+              )}
+            </div>
+            <div className="flex flex-row gap-2 items-center rounded-2xl border border-[#CCCCCC] px-4">
+              <div className="text-xl  text-black/70">₮</div>
+              <input
+                className={`outline-none w-full py-[14px] text-2xl text-primary font-bold`}
+                value={value}
+                type="number"
+                onChange={(e) => {
+                  if (e.target.value == "") {
+                    setValue("");
+                  } else {
+                    setValue(Number(e.target.value));
+                  }
+                }}
+              />
+            </div>
+            <Input
+              label="Имэйл"
+              placeholder="email@domain.com"
+              type="text"
+              defaultValue={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <Button className="py-4 w-full !text-[18px]">Хандив өгөх</Button>
+        </div>
+      </div>
+      <div
+        className={`w-screen h-screen fixed top-0 left-0 ${
+          donateOpen ? "fixed flex" : "hidden"
+        } bg-black/40 items-center justify-center`}
+        onClick={() => setDonateOpen(false)}
+      ></div>
     </>
   );
 };
+const HeartIcon = () => (
+  <svg
+    width="16"
+    height="14"
+    viewBox="0 0 16 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 12.5C8 12.5 1.75 9.00001 1.75 4.75001C1.75 3.99868 2.01031 3.27057 2.48664 2.68954C2.96297 2.10851 3.62589 1.71046 4.36262 1.56312C5.09935 1.41577 5.86438 1.52823 6.52754 1.88136C7.1907 2.23449 7.71103 2.80648 8 3.50001V3.50001C8.28897 2.80648 8.8093 2.23449 9.47246 1.88136C10.1356 1.52823 10.9006 1.41577 11.6374 1.56312C12.3741 1.71046 13.037 2.10851 13.5134 2.68954C13.9897 3.27057 14.25 3.99868 14.25 4.75001C14.25 9.00001 8 12.5 8 12.5Z"
+      fill="#EB547D"
+      stroke="#EB547D"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 // import { FallbackImage } from '@/components/common/fallback-image';
 // import { priceFormat } from '@/lib/utils/price';
 // import {
