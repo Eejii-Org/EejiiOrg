53deac9789e6077518875f85738cd3704217e94d
Enhmunh-E <enhuush0704@gmail.com>
2024-07-13 21:22:21 +0800

    volunteer page responsive

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { MainLayout } from "@/components";
 import { useAuth } from "@/providers";
+import { toDateString } from "@/utils";
 import axios from "axios";
+import { getCookie } from "cookies-next";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -12,9 +14,17 @@ const ProfilePage = () => {
   useEffect(() => {
     const getCertificate = async () => {
       if (!user) return;
+      const token = getCookie("token");
+      if (!token) return;
       try {
+        console.log(user.id);
         const res = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/certificates/${user.id}`
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/certificates`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         console.log(res.data);
         if (res.data.code == 0) {
@@ -37,7 +47,7 @@ const ProfilePage = () => {
   }
   return (
     <MainLayout>
-      <div className="bg-primary  text-white h-64 text-5xl font-semibold">
+      <div className="bg-primary  text-white h-40  md:h-64 text-xl md:text-3xl lg:text-5xl font-semibold">
         <div className="container flex items-center h-full leading-normal">
           Welcome {user.username}!
           <br />
@@ -45,7 +55,7 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="container flex flex-row gap-4 -mt-8">
-        <div className=" w-44 h-44 rounded-full relative overflow-hidden border-[4px] border-white">
+        <div className=" min-h-32 min-w-32 lg:min-w-44 lg:min-h-44 rounded-full relative overflow-hidden border-[4px] border-white">
           <Image
             src={
               user.images?.find((img) => img.type == "main")?.path ||
@@ -56,47 +66,52 @@ const ProfilePage = () => {
           />
         </div>
         <div className="flex flex-col capitalize justify-center gap-3">
-          <div className="text-3xl font-medium">
+          <div className="text-xl lg:text-3xl font-medium">
             {user.firstName} {user.lastName}
           </div>
-          <div className="text-xl">{user.type}</div>
+          <div className="text-lg lg:text-xl">{user.type}</div>
         </div>
       </div>
-      <div className="container max-md:mt-5 pb-[40px] md:py-[60px] flex flex-row gap-8">
+      <div className="container max-md:mt-5 pb-[40px] md:py-[60px] flex flex-col md:flex-row gap-8">
         {/* Left Section for Event Detail */}
-        <div className="w-[360px] flex flex-col gap-4 items-center bg-white p-8 border rounded-2xl">
-          <Image
-            src={
-              user?.level
-                ? `/assets/volunteer/level_${user?.level}.png`
-                : "/assets/placeholder.svg"
-            }
-            alt="volunteerlevel"
-            className="object-contain"
-            height={98}
-            width={98}
-          />
-          <div
-            className={`uppercase py-[2px] px-7 rounded-full text-white font-medium ${
-              user?.level == 1
-                ? "bg-[#1F276F]"
-                : user?.level == 2
-                ? "bg-[#FEC01E]"
-                : user?.level == 3
-                ? "#FD3716"
-                : "#000"
-            }`}
-          >
-            Level {user?.level}
+        <div className="w-full md:w-[360px] flex flex-row lg:flex-col gap-4 lg:items-center bg-white p-4 lg:p-8 border rounded-2xl">
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-16 h-16 lg:w-24 lg:h-24 relative">
+              <Image
+                src={
+                  user?.level
+                    ? `/assets/volunteer/level_${user?.level}.png`
+                    : "/assets/placeholder.svg"
+                }
+                alt="volunteerlevel"
+                fill
+                className="object-contain"
+              />
+            </div>
+            <div
+              className={`uppercase py-[2px] px-3 lg:px-7 whitespace-nowrap max-md:text-sm rounded-full text-white font-medium ${
+                user?.level == 1
+                  ? "bg-[#1F276F]"
+                  : user?.level == 2
+                  ? "bg-[#FEC01E]"
+                  : user?.level == 3
+                  ? "#FD3716"
+                  : "#000"
+              }`}
+            >
+              Level {user?.level}
+            </div>
           </div>
           <div className="w-full flex flex-col gap-3">
-            <div className="border-b w-full pb-1 border-black/20">Био</div>
-            <div>{user.bio}</div>
+            <div className="border-b w-full font-semibold pb-1 border-black/20">
+              Био
+            </div>
+            <div className="text-md lg:text-lg">{user.bio}</div>
           </div>
         </div>
         {/* Right Section for Event Detail */}
-        <div className="flex flex-1 flex-col gap-6 bg-white p-8 border rounded-2xl">
-          <div className="font-semibold text-2xl">Сертификат</div>
+        <div className="flex flex-1 flex-col gap-6 bg-white p-4 lg:p-8 border rounded-2xl">
+          <div className="font-semibold text-lg lg:text-2xl">Сертификат</div>
           <div className="flex-1 flex items-center justify-center flex-col gap-4">
             {!certificateData ? (
               <>
@@ -114,8 +129,32 @@ const ProfilePage = () => {
                 />
               </>
             ) : (
-              <div className="text-center font-medium">
-                Certificate will be here
+              <div className="flex-1 w-full max-h-[500px] overflow-y-scroll font-medium flex flex-col gap-4">
+                {([...certificateData?.["hydra:member"]] as any)?.map(
+                  (certificate: any, index: number) => (
+                    <div
+                      className="flex flex-col gap-2 items-center border p-4 rounded-2xl shadow-sm pointer"
+                      key={index}
+                    >
+                      <div className="w-full flex flex-row gap-2 items-center">
+                        <Image
+                          src={
+                            certificate?.event?.images.find(
+                              (img: any) => img.type == "main"
+                            )?.path || "/assets/placeholder.svg"
+                          }
+                          width={40}
+                          height={40}
+                          alt={certificate?.event?.title + "image"}
+                        />
+                        <div>{certificate?.event?.title}</div>
+                      </div>
+                      <div className=" text-black/70 text-md self-end">
+                        {toDateString(certificate?.event?.startTime)}
+                      </div>
+                    </div>
+                  )
+                )}
               </div>
             )}
           </div>
